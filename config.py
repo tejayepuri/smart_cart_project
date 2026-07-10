6c89e917a80d3f34f9f3bf3e5b989e1787dd0905
Yepuri Teja Naga Anil Kumar <126933993+tejayepuri@users.noreply.github.com>
2026-07-10 10:44:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,15 +8,17 @@
 
 # Stores all configuration settings
 # ------------------------------------------
-
+import os
 SECRET_KEY = "your_secret_key"
 
 # MySQL Database
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASSWORD = "Teja@1610"
-DB_NAME = "smartcart_db"
 
+
+# MySQL Database
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "Teja@1610")
+DB_NAME = os.getenv("DB_NAME", "smartcart_db")
 # Email SMTP Settings
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
